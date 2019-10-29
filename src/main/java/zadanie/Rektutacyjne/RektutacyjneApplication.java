1f90626981a583ecdbaecbafd281c04c7b1afb96
damian12332120 <damian.jedruchniewicz@gmail.com>
2019-10-29 14:15:07 +0100

    Initial commit

---
+++ src/main/java/zadanie/Rektutacyjne/RektutacyjneApplication.java
@@ -0,0 +1,13 @@
+package zadanie.Rektutacyjne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RektutacyjneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RektutacyjneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zadanie/Rektutacyjne/RektutacyjneApplicationTests.java
@@ -0,0 +1,13 @@
+package zadanie.Rektutacyjne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RektutacyjneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
